33fa3c1e0118bb63ae6532e7e5851c2662d56dc6
Noname <>
2025-10-07 14:28:22 +0900

    Refactor start.bat to improve launch sequence for winpeer.exe

--- start.bat
+++ start.bat
@@ -1,6 +1,11 @@
 @echo off
 echo Starting winpeer.exe...
 
+echo Launching from c:\winpeer\
+"c:\winpeer\winpeer.exe"
+echo Exit code: %ERRORLEVEL%
+pause
+
 if not exist "c:\winpeer\winpeer.exe" (
     echo ERROR: winpeer.exe not found at c:\winpeer\winpeer.exe
     pause
